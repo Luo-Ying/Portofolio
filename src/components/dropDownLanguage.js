02373198282252362270a02340e2f73e8c23b032
Luo-Ying <luoyingqifr@gmail.com>
2025-04-25 15:04:41 +0200

    Hide the dropdown menu when clicking outside of it.

--- src/components/dropDownLanguage.js
+++ src/components/dropDownLanguage.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import frenchFlag from '../assets/france.png';
 import ukFlag from '../assets/united-kingdom.png';
 import chinaFlag from '../assets/china.png';
@@ -9,9 +9,24 @@ import { currentLanguage } from '../utils/globalVars';
 const DropDownLanguage = ({ handleLanguageEffect }) => {
 
     const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className="language-selector-circle">
+        <div className="language-selector-circle" ref={dropdownRef}>
             <button className="lang-circle-btn" onClick={() => setShowDropdown(!showDropdown)} >
                 <img
                     src={
